aa99c2399c7f9bfaac1cc78bcf8d4b5c4fd97f1a
Imtxc <txc.yang@gmail.com>
2016-12-31 18:37:03 +0800

    move buy tpscroll funciton to utility

--- utility.lua
+++ utility.lua
@@ -11,6 +11,31 @@ function utility_test()
 end
 
 
+function utility_buy_tpscroll(npcBot)
+
+	local iScrollCount = 0;
+
+	--- buy TP
+	for i=0, 8 do
+		local sCurItem = npcBot:GetItemInSlot(i)
+		if ( sCurItem ~= nil and sCurItem:GetName() == "item_tpscroll") then
+			iScrollCount = iScrollCount + 1;
+		end
+	end
+
+	if ( npcBot:DistanceFromSideShop() == 0 and iScrollCount == 0 ) then
+		npcBot:Action_PurchaseItem("item_tpscroll");
+		npcBot:Action_PurchaseItem("item_tpscroll");
+	end
+
+	if (DotaTime() >= 0 and iScrollCount == 0 and npcBot:DistanceFromFountain() == 0) then
+		npcBot:Action_PurchaseItem("item_tpscroll");
+		npcBot:Action_PurchaseItem("item_tpscroll");
+	end
+
+end
+    
+
 ----------------------------------------------------------------------------------------------------
 
 for k,v in pairs( utility ) do	_G._savedEnv[k] = v end
