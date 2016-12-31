70fb37f118b760e3cc8cc16553b1fdac61eb5a66
Imtxc <txc.yang@gmail.com>
2016-12-31 13:38:33 +0800

    modify wukong's starting items

--- builds/item_build_monkey_king.lua
+++ builds/item_build_monkey_king.lua
@@ -2,28 +2,8 @@
 X = {}
 X["items"] = { 
                 "item_tango",
-                "item_clarity",
-                "item_clarity",
-				--"item_branches",
-				--"item_branches",
-				"item_circlet",
-                "item_mantle",
-                "item_recipe_null_talisman",
-				--"item_bottle",
-				--"item_magic_stick",
-				"item_boots",
-                "item_gloves",
-                "item_belt_of_strength",
-				--"item_energy_booster",
-                "item_ring_of_regen",
-				"item_staff_of_wizardry",
-				"item_recipe_force_staff",
-                "item_blink",
-				"item_point_booster",
-				"item_staff_of_wizardry",
-				"item_ogre_axe",
-				"item_blade_of_alacrity"
-			};
+		"item_poor_mans_shield"
+	};
 
 -- Set up Skill build
 local SKILL_Q = "monkey_king_boundless_strike";
