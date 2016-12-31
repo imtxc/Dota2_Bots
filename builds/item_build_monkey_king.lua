4a02db061dd0b688e1ac581a352ff6512656c673
Imtxc <txc.yang@gmail.com>
2016-12-31 13:31:15 +0800

    modify monkey king's ability name

--- builds/item_build_monkey_king.lua
+++ builds/item_build_monkey_king.lua
@@ -1,8 +1,5 @@
 -- http://dota2.gamepedia.com/Monkey_King
 X = {}
---[[ warning if meepo does not have an item other than 
-brown boots / power treads at any time he will think he 
-is a clone and skill/item decisions will break! ]]
 X["items"] = { 
                 "item_tango",
                 "item_clarity",
@@ -35,15 +32,14 @@ local SKILL_E = "monkey_king_jingu_mastery";
 local SKILL_R = "monkey_king_mischief";    
 local SKILL_D = "monkey_king_primal_spring_early";    -- no levels
 
-
-local ABILITY1 = "special_bonus_intelligence_8"
-local ABILITY2 = "special_bonus_hp_150"
-local ABILITY3 = "special_bonus_attack_damage_40"
+local ABILITY1 = "special_bonus_attack_speed_20"
+local ABILITY2 = "special_bonus_armor_5"
+local ABILITY3 = "special_bonus_hp_275"
 local ABILITY4 = "special_bonus_movement_speed_20"
-local ABILITY5 = "special_bonus_spell_amplify_8"
-local ABILITY6 = "special_bonus_magic_resistance_20"
-local ABILITY7 = "special_bonus_gold_income_50"
-local ABILITY8 = "special_bonus_unique_puck"
+local ABILITY5 = "special_bonus_attack_damage_40"
+local ABILITY6 = "special_bonus_strength_15"
+local ABILITY7 = "special_bonus_magic_resistance_25"
+local ABILITY8 = "special_bonus_unique_monkey_king"
 
 --use -1 for levels that shouldn't level a skill
 X["skills"] = {
